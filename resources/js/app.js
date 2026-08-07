b7f84667064eed179f8e5de1c730654b20385ff8
Ferdyan <ferdyan.darwis.fd@gmail.com>
2026-08-07 15:24:08 +0700

    add: fix bug laoder/loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -660,21 +660,6 @@ window.addEventListener("scroll",()=>{
         LOADING EFFECT
 ========================================== */
 
-window.onload=()=>{
-
-    document.body.style.opacity="0";
-
-    setTimeout(()=>{
-
-        document.body.style.transition="1s";
-
-        document.body.style.opacity="1";
-
-    },100);
-
-
-    
-};
 
 const resetFilterButton =
     document.getElementById('resetMapFilter');
