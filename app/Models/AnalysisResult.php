e1cb7eeb1d7bc2ec43674619dd8cfff3c0b44e0d
Alfi-Syahrin21 <alfioke7@gmail.com>
2026-08-13 15:42:28 +0700

    feat: Ubah Kolom tabel analisis

--- app/Models/AnalysisResult.php
+++ app/Models/AnalysisResult.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AnalysisResult extends Model
 {
@@ -13,4 +14,9 @@ class AnalysisResult extends Model
     protected $casts = [
         'results' => 'array',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
